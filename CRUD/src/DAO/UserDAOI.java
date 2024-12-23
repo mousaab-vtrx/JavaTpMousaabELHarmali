23a11e7d4c726c09ec4e3564005bf084d9c1d1e7
Mousaab El harmali <mousaabelharmali31@gmail.com>
2024-12-23 12:27:07 +0100

    Delete CRUD/src directory

--- CRUD/src/DAO/UserDAOI.java
+++
@@ -1,9 +0,0 @@
-package DAO;
-import Model.*;
-
-public interface UserDAOI {
-    void addUser(User usr);
-    boolean checkUser(User usr);
-    int getUsersIds(String username, String password);
-    boolean usernameExists(String username);
-}
